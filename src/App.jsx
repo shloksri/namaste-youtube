6240d24a34cf11fc530cd669dafb5a2f8063f2bc
Shlok <shloksrivastava7@gmail.com>
2025-02-12 13:16:00 +0530

    all components created, header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import Body from "./components/Body";
+import Head from "./components/Head";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Namaste Youtube</h1>
-      <span>Content</span>
+      {/* <h1 className="text-3xl font-bold">Namaste Youtube</h1> */}
+
+      <Head />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Head.jsx
@@ -0,0 +1,36 @@
+const Head = () => {
+  return (
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-8"
+          alt="Menu"
+          src="https://icons.veryicon.com/png/o/miscellaneous/linear-icon-45/hamburger-menu-4.png"
+        />
+        <img
+          className="h-8 mx-2"
+          alt="youtube-logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/2560px-YouTube_Logo_2017.svg.png"
+        />
+      </div>
+      <div className="col-span-10 px-10 text-center">
+        <input
+          type="text"
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+        />
+        <button className="border border-gray-400 p-2 rounded-r-full px-5 bg-gray-100">
+          🔍
+        </button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          alt="user"
+          src="https://openclipart.org/image/2000px/247319"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Head;
