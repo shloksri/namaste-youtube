ac0afbedb836549358887b379f7a95ee0d92d43e
Shlok <shloksrivastava7@gmail.com>
2025-02-14 19:06:28 +0530

    Nested Comments List completed

---
+++ src/components/CommentsContainer.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+
+const commentsData = [
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [
+              {
+                name: "Akshay Saini",
+                text: "Lorem ipsum dolor sit amet, consectetur adip",
+                replies: [
+                  {
+                    name: "Akshay Saini",
+                    text: "Lorem ipsum dolor sit amet, consectetur adip",
+                    replies: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [],
+      },
+    ],
+  },
+  {
+    name: "Akshay Saini",
+    text: "Lorem ipsum dolor sit amet, consectetur adip",
+    replies: [
+      {
+        name: "Akshay Saini",
+        text: "Lorem ipsum dolor sit amet, consectetur adip",
+        replies: [
+          {
+            name: "Akshay Saini",
+            text: "Lorem ipsum dolor sit amet, consectetur adip",
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data;
+  return (
+    <div className="flex shadow-sm bg-gray-100 p-2 rounded-lg my-2">
+      <img
+        className="w-12 h-12"
+        alt="user"
+        src="https://openclipart.org/image/2000px/247319"
+      />
+      <div className="px-3">
+        <p className="font-bold">{name}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+const CommentsList = ({ comments }) => {
+  return comments.map((comment, index) => (
+    <div key={index}>
+      <Comment data={comment} />
+      <div className="pl-5 border border-l-black ml-5">
+        <CommentsList comments={comment.replies} />
+      </div>
+    </div>
+  ));
+};
+
+const CommentsContainer = () => {
+  return (
+    <div className="m-2 p-2">
+      <h1 className="text-2xl font-bold">Comments:</h1>
+      <CommentsList comments={commentsData} />
+    </div>
+  );
+};
+
+export default CommentsContainer;
--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -15,13 +16,16 @@ const WatchPage = () => {
 
   const collapseSidebar = () => {};
   return (
-    <div className="p-5">
-      <iframe
-        width="1200"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+    <div className="flex flex-col">
+      <div className="p-5">
+        <iframe
+          width="1100"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+      </div>
+      <CommentsContainer />
     </div>
   );
 };
